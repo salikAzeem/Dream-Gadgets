426bd2dfb09b9280e39219704a4c643480f1558b
salik azeem <salikazeem123@gmail.com>
2026-01-05 23:24:04 +0530

    rsht

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const productRoutes = require("./routes/productRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
@@ -13,13 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-mongoose
-  .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error("Mongo error:", err));
+  .catch(err => console.error("Mongo Error:", err));
 
-app.use("/api/products", productRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
 const PORT = process.env.PORT || 5000;
