eaa4232584c32a33e96c4836fe9b699d462a5685
salik azeem <salikazeem123@gmail.com>
2026-01-06 00:45:40 +0530

    commm

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,13 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
 
+// 🔥 THIS IS THE MOST IMPORTANT PART
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("adminToken");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
 export default API;
